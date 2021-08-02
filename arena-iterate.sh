f1209bed043e57cd288b6a24a605e9a619f71a49
Todd Hodes <todd@locationlabs.com>
2021-08-01 20:07:40 -0700

    automate arena
    -:   arena-buy-refills.sh
    -:   arena-fight5.sh
    -:   buy-50kcoin-pack.sh
    
    -:   arena-iterate.sh

---
+++ arena-iterate.sh
@@ -0,0 +1,17 @@
+
+case "$1" in
+"") count=1 ;;
+*) count=$1 ;;
+esac
+
+for i in `nums $count`
+do
+  echo "buy 150"
+  ./arena-buy-refills.sh 15
+  sleep 5	
+  echo "fight 150"
+  ./arena-fight5.sh 30 
+  sleep 3	
+  echo "buy coin packs"
+  ./buy-50kcoin-pack.sh 9
+done
