5d1a5f14d48e14b37b4cf0537f0102bd330159d2
Todd Hodes <todd@toddh.org>
2023-01-03 01:03:26 -0800

    add starting energy arg

--- arena-sfc-to-rank.sh
+++ arena-sfc-to-rank.sh
@@ -4,18 +4,30 @@
 #
 
 case "$1" in
-"") min=2350 ;;
+"") echo "Usage: $0 <min-SFC-rank> [start-energy-available]" ; exit  ;;
 *) min=$1 ;;
 esac
 
+case "$2" in
+"") energy=0 ;;
+*) energy=$2 ;;
+esac
+
+echo "fight arena until SFC $min"
 for i in `seq 1 1000`
 do
-  echo "iteration $i of $count"
-  echo "buy a refill"
-  ./arena-buy-refills.sh 1
-  sleep 5	
-  echo "fight up to 10"
-  for j in `seq 1 10`
+  echo "iteration $i with energy $energy"
+
+  if [[ $energy = 0 ]]
+  then
+    echo "buy a refill"
+    ./arena-buy-refills.sh 1
+    energy=10
+    sleep 5	
+  fi
+
+  echo "fight up to $energy"
+  for j in `seq 1 $energy`
   do
     ./arena-fight5.sh 1 1
     sfc=`jq .pvp_data.rating < o-auto_battle`
@@ -25,5 +37,6 @@ do
       echo "done: SFC now $sfc"
       exit
     fi
+    energy=$((energy-1))
   done
 done
