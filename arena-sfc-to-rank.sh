501d2cbf8211ebe18dd2e62f2053276427b64b15
Todd Hodes <todd.hodes@eluv.io>
2024-03-24 01:10:53 -0700

    allow optional fights

--- arena-sfc-to-rank.sh
+++ arena-sfc-to-rank.sh
@@ -1,16 +1,21 @@
-
+#!/bin/bash
 #
 # play unitl SFC > arg
 #
 
 case "$1" in
-"") echo "Usage: $0 <min-SFC-rank> [start-energy-available]" ; exit  ;;
+"") echo "Usage: $0 <min-SFC-rank> [start-energy-available] [fights]" ; exit  ;;
 *) min=$1 ;;
 esac
 
 case "$2" in
 "") energy=0 ;;
 *) energy=$2 ;;
+esac
+
+case "$3" in
+"") fights=1 ;;
+*) fights=$3 ;;
 esac
 
 echo "fight arena until SFC $min"
@@ -26,10 +31,10 @@ do
     sleep 5	
   fi
 
-  echo "fight up to $energy"
-  for j in `seq 1 $energy`
+  echo "fight up to $energy at $fights per, count $((energy/fights))"
+  for j in `seq 1 $((energy/fights))`
   do
-    ./arena-fight5.sh 1 1
+    ./arena-fight5.sh 1 ${fights}
     sfc=`jq .pvp_data.rating < o-auto_battle`
     echo "new sfc: $sfc"
     if [[ $sfc > $min ]]
@@ -37,6 +42,6 @@ do
       echo "done: SFC now $sfc"
       exit
     fi
-    energy=$((energy-1))
+    energy=$((energy-fights))
   done
 done
