bfdae6a2600e1f8c3c54a66acf7a734d0ed12294
Todd Hodes <todd.hodes@eluv.io>
2026-09-17 17:46:53 -0700

    alow more than one arg

--- cardAndCmGrep.sh
+++ cardAndCmGrep.sh
@@ -1,1 +1,11 @@
-grep -h "$@" Combos/ComboMastery Decks/CARDS
+pattern=$1
+shift
+
+result=`grep -h "$pattern" Combos/ComboMastery Decks/CARDS`
+
+for p in "$@"
+do
+  result=`echo "$result" | grep "$p"`
+done
+
+echo "$result"
